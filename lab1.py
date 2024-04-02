eceabc2676e8e47a1b5fed0ce514ff47b033d050
Nikon Parvitskiy <Nikon.ser.par@gmail.com>
2024-04-02 22:16:02 +0300

    refactor

--- lab1.py
+++ lab1.py
@@ -1,33 +1,50 @@
 import matplotlib.pyplot as painter
 import math
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
 
+    @property
+    def x(self):
+        return self.__x
+    
+    @property
+    def y(self):
+        return self.__y
 
-# def start(i, formula):
-#     return painter.Rectangle(((i+1)*step_len, 0), step_len, formula, edgecolor='r', facecolor='black')
+def get_formula_value_at(x: int):
+    return math.e ** x
 
-# def end(i, formula):
-#     return painter.Rectangle((i*step_len, 0), step_len, formula, edgecolor='r', facecolor='black')
+def calculate_graph_points(start: int, end: int, accuracy: int) -> list[Point]:
+    points = []
+    step = (end - start) / accuracy
+    x = start
+    while (x < end):
+        points.append(Point(x, get_formula_value_at(x)))
+        x += step
 
-# def middle():
-#     return 0
+    return points
 
+def create_integral_shape(points: list[Point], step: int) -> list[painter.Rectangle]:
+    rectangles = []
+    for point in points:
+        rectangles.append(painter.Rectangle((point.x, 0), step, point.y, edgecolor='r', facecolor='black'))
 
- 
+    return rectangles
+        
+def create_window():
+    painter.xlim(0, 3)
+    painter.ylim(0, 3)
+    painter.grid(True)
+    painter.show()
 
-n = 100
-start = 1
-end = 2
-step_len = (end - start) / n
-x = start
-while (x < end):
-    formula = math.e ** x # формула
-    rectangle = painter.Rectangle((x, 0), step_len, formula, edgecolor='r', facecolor='black')
-    x += step_len
-    painter.gca().add_patch(rectangle)
+def main():
+    points = calculate_graph_points(0, 1, 100)
+    rectangles = create_integral_shape(points, 100)
+    for rectangle in rectangles:
+        painter.gca().add_patch(rectangle)
+    create_window()
 
-# координаты начала, длина, высота, красота 
-painter.xlim(0, 2)
-painter.ylim(0, 10)
-painter.grid(True)
-painter.show()
+main()
