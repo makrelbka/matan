41684f6d82a86bb2ab7ed555dd82c64423e0d9f5
Nikon Parvitskiy <Nikon.ser.par@gmail.com>
2024-04-03 00:07:09 +0300

    e^x draw

--- lab.py
+++ lab.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as painter
 import math
+import random
+import sys
+from enum import Enum
+
+class Equipment(Enum):
+    LEFT = 1
+    RIGHT = 2
+    MIDDLE = 3
+    RANDOM = 4
+
 
 class Point:
     def __init__(self, x: int, y: int):
@@ -17,40 +27,60 @@ class Point:
 def get_formula_value_at(x: int) -> int:
     return math.e ** x
 
-def calculate_step(start: int, end: int, accuracy: int) -> int:
+def calculate_delta_x(start: int, end: int, accuracy: int) -> int:
     return (end - start) / accuracy
 
 def calculate_graph_points(start: int, end: int, accuracy: int) -> list[Point]:
     points = []
 
-    step = calculate_step(start, end, accuracy)
-    x = start
-    while (x < end):
+    delta_x = calculate_delta_x(start, end, accuracy)
+    for i in range(accuracy + 1):
+        x = start + i * delta_x
         points.append(Point(x, get_formula_value_at(x)))
-        x += step
 
     return points
 
-def create_integral_shape(points: list[Point], accuracy: int) -> list[painter.Rectangle]:
+def calculate_integral_shape_shift(delta_x: int, equipment: Equipment):
+    match equipment:
+        case Equipment.LEFT:   return 0
+        case Equipment.RIGHT:  return delta_x
+        case Equipment.MIDDLE: return delta_x / 2
+        case Equipment.RANDOM: return random.uniform(0, delta_x)
+        case _:                raise Exception('Invalid equipment')
+
+def create_integral_rectangle(point: Point, delta_x: int, delta_x_shift: int) -> painter.Rectangle:
+    return painter.Rectangle((point.x - delta_x_shift, 0), delta_x, point.y, edgecolor='r', facecolor='black')
+
+def create_integral_shape(points: list[Point], accuracy: int, equipment: Equipment) -> list[painter.Rectangle]:
     rectangles = []
 
-    step = calculate_step(points[0].x, points[-1].x, accuracy)
+    delta_x = calculate_delta_x(points[0].x, points[-1].x, accuracy)
+    delta_x_shift = calculate_integral_shape_shift(delta_x, equipment)
     for point in points:
-        rectangles.append(painter.Rectangle((point.x, 0), step, point.y, edgecolor='r', facecolor='black'))
+        rectangles.append(create_integral_rectangle(point, delta_x, delta_x_shift))
 
     return rectangles
-        
-def create_window() -> None:
-    painter.xlim(0, 3)
-    painter.ylim(0, 3)
+
+def create_window(x_axis_size: tuple[int, int], y_axis_size: tuple[int, int]) -> None:
+    painter.xlim(x_axis_size[0], x_axis_size[1])
+    painter.ylim(y_axis_size[0], y_axis_size[1])
     painter.grid(True)
     painter.show()
 
-def main(start: int, end: int, accuracy: int):
+def draw_integral_sum(start: int, end: int, accuracy:int, equipment: Equipment) -> None:
     points = calculate_graph_points(start, end, accuracy)
-    rectangles = create_integral_shape(points, accuracy)
+    rectangles = create_integral_shape(points, accuracy, equipment)
     for rectangle in rectangles:
         painter.gca().add_patch(rectangle)
-    create_window()
+    create_window((start, end), (0, max(point.y for point in points)))
 
-main(0, 1, 100)
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print('Usage: python3 lab.py <start> <end> <accuracy> <equipment>')
+        sys.exit(1)
+
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    accuracy = int(sys.argv[3])
+    equipment = Equipment(int(sys.argv[4]))
+    draw_integral_sum(start, end, accuracy, equipment)
